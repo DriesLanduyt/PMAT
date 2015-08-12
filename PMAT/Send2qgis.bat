2ae07a825e69c9b285c66635a64c6888cc3af9bb
DriesLanduyt <drieslanduyt@gmail.com>
2015-08-12 23:28:04 +0200

    Created Send2qgis file
    Batch file that copies most recent developments to qgis to test plug-in
    within qgis

---
+++ PMAT/Send2qgis.bat
@@ -0,0 +1,4 @@
+copy ".\probabilistic_map_algebra_tool_dialog_base.py" "C:\OSGeo4W64\apps\qgis\python\plugins\PMAT\"
+copy ".\probabilistic_map_algebra_tool.py" "C:\OSGeo4W64\apps\qgis\python\plugins\PMAT\"
+copy ".\NeticaWrapper.py" "C:\OSGeo4W64\apps\qgis\python\plugins\PMAT\"
+pause
